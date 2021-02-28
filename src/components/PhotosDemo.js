c9d230f94f735687a88ac01d098102605241297f
alex rodriguez <alexcarvajal2404@gmail.com>
2021-02-28 18:21:18 -0500

    28/02/2021

--- src/components/PhotosDemo.js
+++ src/components/PhotosDemo.js
@@ -1,101 +1,109 @@
-import React, { useEffect, useState } from 'react';
-import { Carousel } from 'primereact/carousel';
-import { Galleria } from 'primereact/galleria';
-import { Button } from 'primereact/button';
-import ProductService from '../service/ProductService';
-import PhotoService from '../service/PhotoService';
+import React, { useState, useEffect, useRef } from 'react'
+import { DataView } from 'primereact/dataview';
+import { Button } from "primereact/button"
+import { SplitButton } from 'primereact/splitbutton'
+import { Toast } from 'primereact/toast';
+import { Card } from 'primereact/card'
+import ProductService from '../service/ProductService' 
 
 export const PhotosDemo = () => {
-
-    const [products, setProducts] = useState([]);
-    const [images, setImages] = useState([]);
-    const galleriaResponsiveOptions = [
-        {
-            breakpoint: "1024px",
-            numVisible: 5,
-        },
-        {
-            breakpoint: "960px",
-            numVisible: 4,
-        },
-        {
-            breakpoint: "768px",
-            numVisible: 3,
-        },
-        {
-            breakpoint: "560px",
-            numVisible: 1,
-        },
-    ];
-    const carouselResponsiveOptions = [
-        {
-            breakpoint: "1024px",
-            numVisible: 3,
-            numScroll: 3,
-        },
-        {
-            breakpoint: "768px",
-            numVisible: 2,
-            numScroll: 2,
-        },
-        {
-            breakpoint: "560px",
-            numVisible: 1,
-            numScroll: 1,
-        },
-    ];
+    const [ dataViewValue, setDataViewValue ] = useState(null)
+    const [ layout, setLayout ] = useState('grid')
+    const  toast = useRef()
 
     useEffect(() => {
-        const productService = new ProductService();
-        productService.getProductsSmall().then((products) => setProducts(products));
+        const planService = new ProductService();
+        planService.getProducts().then((data) => setDataViewValue(data));
+    }, [])
 
-        const photoService = new PhotoService();
-        photoService.getImages().then((images) => setImages(images));
-    }, []);
+    const items = [
+        {
+            label: 'Update',
+            icon: 'pi pi-refresh',
+            command: (e) => {
+                toast.current.show({severity:'success', summary:'Updated', detail:'Data Updated'});
+            }
+        },
+        {
+            label: 'Delete',
+            icon: 'pi pi-times',
+            command: (e) => {
+                toast.current.show({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
+            }
+        },
+        {
+            label: 'React Website',
+            icon: 'pi pi-external-link',
+            command:(e) => {
+                window.location.href = 'https://facebook.github.io/react/'
+            }
+        },
+        {   label: 'Upload',
+            icon: 'pi pi-upload',
+            disabled: true
+        }
+    ]
 
-    const carouselItemTemplate = (product) => {
-        return (
-            <div className="product-item">
-                <div className="product-item-content">
-                    <div className="p-mb-3">
-                        <img src={`assets/demo/images/product/${product.image}`} alt={product.name} className="product-image" />
-                    </div>
-                    <div>
-                        <h4 className="p-mb-1">
-                            {product.name}
-                        </h4>
-                        <h6 className="p-mt-0 p-mb-3">${product.price}</h6>
-                        <span className={`product-badge status-${product.inventoryStatus.toLowerCase()}`}>{product.inventoryStatus}</span>
-                        <div className="car-buttons p-mt-5">
-                            <Button type="button" className="p-button p-button-rounded p-mr-2" icon="pi pi-search"></Button>
-                            <Button type="button" className="p-button-success p-button-rounded p-mr-2" icon="pi pi-star"></Button>
-                            <Button type="button" className="p-button-help p-button-rounded" icon="pi pi-cog"></Button>
-                        </div>
-                    </div>
-                </div>
+    const cardHeader = (data) => {
+        return(
+            <img src={data.image} alt={data.name} />
+        )
+    }
+
+    const dataViewHeader = (
+        <div className="p-grid p-nogutter">
+            <div className="p-col-12 p-md-6" style={{ textAlign: 'left' }}>
+                <Button label="New Plan" icon="pi pi-plus" className="p-button-text p-mr-2" />
+                <Button label="New Folder" icon="pi pi-plus" className="p-button-text p-button-plain p-mr-2" />
+                <SplitButton label="Actions" model={items} className="p-button-warning"></SplitButton>
             </div>
-        );
-    };
-
-    const galleriaItemTemplate = (item) => <img src={`assets/${item.itemImageSrc}`} alt={item.alt} style={{ width: '100%', display: 'block' }} />
-    const galleriaThumbnailTemplate = (item) => <img src={`assets/${item.thumbnailImageSrc}`} alt={item.alt} style={{ width: '100%', display: 'block' }} />
-
-    return (
-        <div className="p-grid p-fluid media-demo">
-            <div className="p-col-12">
-                <div className="card">
-                    <h5>Carousel</h5>
-                    <Carousel value={products} numVisible={3} numScroll={3} responsiveOptions={carouselResponsiveOptions} itemTemplate={carouselItemTemplate}></Carousel>
-                </div>
-            </div>
-
-            <div className="p-col-12">
-                <div className="card">
-                    <h5>Galleria</h5>
-                    <Galleria value={images} responsiveOptions={galleriaResponsiveOptions} numVisible={7} circular style={{ maxWidth: '800px', margin: 'auto' }}
-                        item={galleriaItemTemplate} thumbnail={galleriaThumbnailTemplate}></Galleria>
-                </div>
+            <div className="p-col-12 p-md-6 p-d-flex p-jc-end p-ai-center"   style={{ textAlign: 'right' }}>
+                <Button label="Filter Plans" icon="pi pi-filter" className="p-button-plain  p-button-text p-mr-2" />
+                <Button label="Version Control" className="p-button-plain p-button-text p-mr-2" />
+                {/* <DataViewLayoutOptions layout={layout} onChange={(e) => setLayout(e.value)} /> */}
             </div>
         </div>
     );
+
+    
+    const dataViewGridItem = (data) => {
+        return (
+            <div className="p-grid p-d-flex p-flex-wrap">
+                    <Card style={{minWidth: '150px', margin:"32px", padding:"10px", minHeight: '150px'}} header={cardHeader(data)} />
+                </div>
+        )
+    }
+    
+    const dataViewListItem = (data) => {
+        return (
+            <div className="p-col-12">
+                 {data.name}
+            </div>
+        )
+    } 
+
+
+
+    const itemTemplate = (data, layout) => {
+        if (!data) {
+            return;
+        }
+
+        if (layout === 'list') {
+            return dataViewListItem(data);
+        }
+        else if (layout === 'grid') {
+            return dataViewGridItem(data);
+        }
+    };
+    return (
+        <>
+        <Toast ref={toast}></Toast>
+            <div className="p-grid">
+                <div className="p-col-12">
+                        <DataView value={dataViewValue} layout={"grid"} itemTemplate={itemTemplate} header={dataViewHeader}></DataView>
+                </div>
+            </div>
+        </>
+    )
 }
--- src/components/TasksDemo.js
+++ src/components/TasksDemo.js
@@ -1,151 +1,218 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
-import { Button } from "primereact/button"
-import { SplitButton } from 'primereact/splitbutton'
-import { Toast } from 'primereact/toast';
-import TaskService from '../service/TaskService' 
+// import React, { useRef, useEffect, useState } from 'react';
+// import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
+// import { Button } from "primereact/button"
+// import { SplitButton } from 'primereact/splitbutton'
+// import { Toast } from 'primereact/toast';
+// import TaskService from '../service/TaskService' 
+
+
+// export const TasksDemo = () => {
+//     const [ dataViewValue, setDataViewValue ] = useState(null)
+//     const [ layout, setLayout ] = useState('grid')
+//     // const [ data, SetData ] = useState([])
+//     const  toast = useRef()
+
+//     useEffect(() => {
+//         const taskService = new TaskService();
+//         taskService.getTasks().then(({data}) => setDataViewValue(data));
+//     }, [])
+
+//     const items = [
+//         {
+//             label: 'Update',
+//             icon: 'pi pi-refresh',
+//             command: (e) => {
+//                 toast.current.show({severity:'success', summary:'Updated', detail:'Data Updated'});
+//             }
+//         },
+//         {
+//             label: 'Delete',
+//             icon: 'pi pi-times',
+//             command: (e) => {
+//                 toast.current.show({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
+//             }
+//         },
+//         {
+//             label: 'React Website',
+//             icon: 'pi pi-external-link',
+//             command:(e) => {
+//                 window.location.href = 'https://facebook.github.io/react/'
+//             }
+//         },
+//         {   label: 'Upload',
+//             icon: 'pi pi-upload',
+//             disabled: true
+//         }
+//     ]
+
+//     const dataViewHeader = (
+//         <div className="p-grid p-nogutter">
+//             <div className="p-col-12 p-md-6" style={{ textAlign: 'left' }}>
+//                 <Button label="New task" icon="pi pi-plus" className="p-button-text p-mr-2" />
+//                 <Button label="Important Tasks" icon="pi pi-plus" className="p-button-text p-button-plain p-mr-2" />
+//                 <Button label="Generate Reports" icon="pi pi-print" className="p-button-text p-button-plain p-mr-2" />
+//                 <SplitButton label="Actions" model={items} className="p-button-warning"></SplitButton>
+//             </div>
+//             <div className="p-col-12 p-md-6 p-d-flex p-jc-end p-ai-center"   style={{ textAlign: 'right' }}>
+//                 <Button label="Sort Tasks" icon="pi pi-sort-alpha-down" className="p-button-plain  p-button-text p-mr-2" />
+//                 <Button label="Filter Tasks" icon="pi pi-filter" className="p-button-plain  p-button-text p-mr-2" />
+//                 <DataViewLayoutOptions layout={layout} onChange={(e) => setLayout(e.value)} />
+//             </div>
+//         </div>
+//     );
+
+//     const dataViewListItem = () => {
+//         return (
+//             <div>
+//                 list
+//             </div>
+//         )
+//     } 
+
+//     const dataViewGridItem = () => {
+//         return (
+//             <div>
+//                 Grid
+//         </div>
+//         )
+//     }
+    
+//     const Example  = (data) => {
+//     return (
+//             <div>
+//                 {
+//                     data.map(item => (
+//                         <div key={item.id}>
+//                             {item.title}
+//                             {
+//                                 item.cards.map(card => (
+//                                     <p key={card.id ``}>
+//                                         {card.text}
+//                                     </p>
+//                                 ))
+//                             }
+//                         </div>
+//                     ))
+//                 }
+//             </div>
+//         )
+//     } 
+
+//     const itemTemplate = (data, layout) => {
+//         if (!data) {
+//             return;
+//         }
+
+//         if (layout === 'list') {
+//             return dataViewListItem(data);
+//         }
+//         else if (layout === 'grid') {
+//             return dataViewGridItem(data);
+//         }
+//     };
+
+
+//     return (
+//         <>
+//         <Toast ref={toast}></Toast>
+//             <div className="p-grid">
+//                 <div className="p-col-12">
+//                         <DataView value={dataViewValue} layout={layout} itemTemplate={itemTemplate} header={dataViewHeader}></DataView>
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+
+import React, { useState, useEffect } from 'react'
+import TaskService from '../service/TaskService'
+import styled from 'styled-components'
+import { DragDropContext } from 'react-beautiful-dnd'
+import { Droppable } from 'react-beautiful-dnd'
+import { Draggable } from 'react-beautiful-dnd'
+
+const CardContainerWrapper = styled.div`
+    margin: 8px;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+`
+const Title = styled.h3`
+    padding: 8px;
+`
+const TaskList = styled.div`
+    padding: 8px;
+`
+
+const CardWrapper = styled.div`
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+    padding: 8px;
+    margin-bottom: 8px;
+`
 
 
 export const TasksDemo = () => {
-    const [ dataViewValue, setDataViewValue ] = useState(null)
-    const [ layout, setLayout ] = useState('grid')
-    // const [ data, SetData ] = useState([])
-    const  toast = useRef()
+    const [exampleData, setExampleData] = useState([])
+
+    const dragEnd = result => {
+        console.log("draggind and dropping")
+    }
 
     useEffect(() => {
-        const taskService = new TaskService();
-        taskService.getTasks().then(({data}) => setDataViewValue(data));
-    }, [])
+        const service = new TaskService()
+        service.getTasks().then(data => setExampleData(data))
+    },[])
 
-    const items = [
-        {
-            label: 'Update',
-            icon: 'pi pi-refresh',
-            command: (e) => {
-                toast.current.show({severity:'success', summary:'Updated', detail:'Data Updated'});
-            }
-        },
-        {
-            label: 'Delete',
-            icon: 'pi pi-times',
-            command: (e) => {
-                toast.current.show({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
-            }
-        },
-        {
-            label: 'React Website',
-            icon: 'pi pi-external-link',
-            command:(e) => {
-                window.location.href = 'https://facebook.github.io/react/'
-            }
-        },
-        {   label: 'Upload',
-            icon: 'pi pi-upload',
-            disabled: true
-        }
-    ]
-
-    const dataViewHeader = (
-        <div className="p-grid p-nogutter">
-            <div className="p-col-12 p-md-6" style={{ textAlign: 'left' }}>
-                <Button label="New task" icon="pi pi-plus" className="p-button-text p-mr-2" />
-                <Button label="Important Tasks" icon="pi pi-plus" className="p-button-text p-button-plain p-mr-2" />
-                <Button label="Generate Reports" icon="pi pi-print" className="p-button-text p-button-plain p-mr-2" />
-                <SplitButton label="Actions" model={items} className="p-button-warning"></SplitButton>
-            </div>
-            <div className="p-col-12 p-md-6 p-d-flex p-jc-end p-ai-center"   style={{ textAlign: 'right' }}>
-                <Button label="Sort Tasks" icon="pi pi-sort-alpha-down" className="p-button-plain  p-button-text p-mr-2" />
-                <Button label="Filter Tasks" icon="pi pi-filter" className="p-button-plain  p-button-text p-mr-2" />
-                <DataViewLayoutOptions layout={layout} onChange={(e) => setLayout(e.value)} />
-            </div>
-        </div>
-    );
-
-    const dataViewListItem = (data) => {
+    const CardContainer = ({ title, children, id}) => {
         return (
-            <div>
-                {
-                    data.map(item => (
-                        <div key={item.id}>
-                            {item.title}
-                            {
-                                item.cards.map(card => (
-                                    <p key={card.id ``}>
-                                        {card.text}
-                                    </p>
-                                ))
-                            }
-                        </div>
-                    ))
-                }
-            </div>
-        )
-    } 
-
-    const dataViewGridItem = (data) => {
-        return (
-            <div>
-            {
-                data.map(item => (
-                    <div key={item.id}>
-                        {item.title}
-                        {
-                            item.cards.map(card => (
-                                <p key={card.id ``}>
-                                    {card.text}
-                                </p>
-                            ))
-                        }
-                    </div>
-                ))
-            }
-        </div>
+            <CardContainerWrapper>
+                <Title>{title}</Title>
+                <Droppable droppableId={id}>
+                    {
+                        (provided) => (
+                            <TaskList {...provided.droppableProps} innerRef={provided.innerRef}>
+                                {children}
+                                {provided.placeholder}
+                            </TaskList>
+                        )
+                    }
+                </Droppable>
+            </CardContainerWrapper>
         )
     }
-    
-    const Example  = (data) => {
-    return (
-            <div>
+
+    const Card = ({ children, id, index }) => {
+        return (
+            <Draggable draggableId={id} index={index}>
                 {
-                    data.map(item => (
-                        <div key={item.id}>
-                            {item.title}
-                            {
-                                item.cards.map(card => (
-                                    <p key={card.id ``}>
-                                        {card.text}
-                                    </p>
-                                ))
-                            }
-                        </div>
-                    ))
+                    (provided) => (
+                        <CardWrapper {...provided.draggableProps} {...provided.dragHandleProps} innerRef={provided.innerRef}>
+                            {children}
+                        </CardWrapper>
+                    )
                 }
-            </div>
+            </Draggable>
         )
     } 
 
-    const itemTemplate = (data, layout) => {
-        if (!data) {
-            return;
-        }
-
-        if (layout === 'list') {
-            return dataViewListItem(data);
-        }
-        else if (layout === 'grid') {
-            return dataViewGridItem(data);
-        }
-    };
-
+    
 
     return (
-        <>
-        <Toast ref={toast}></Toast>
-            <div className="p-grid">
-                <div className="p-col-12">
-                        <DataView value={dataViewValue} layout={layout} itemTemplate={itemTemplate} header={dataViewHeader}></DataView>
-                </div>
-            </div>
-        </>
+        <DragDropContext  onDragEnd={dragEnd}>
+            {
+                exampleData.map(data => (
+                    <CardContainer key={data.id} id={data.id} title={data.title}>
+                        {
+                            data.cards.map((card, index) => (
+                                <Card key={card.id} id={card.id} index={index}>
+                                    {card.text}
+                                </Card>
+                            ))
+                        }
+                    </CardContainer>
+                ))
+            }
+        </DragDropContext>
     )
 }
